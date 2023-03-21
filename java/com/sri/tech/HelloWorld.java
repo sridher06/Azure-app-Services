3c5e0a672e3d65d2085f76b4eb2cf0f470c57e32
sridher06 <128428483+sridher06@users.noreply.github.com>
2023-03-21 20:48:47 +0000

    Update HelloWorld.java

--- java/com/sri/tech/HelloWorld.java
+++ java/com/sri/tech/HelloWorld.java
@@ -1,9 +1,26 @@
 package com.sri.tech;
 
-public class HelloWorld {
-	
-	public void helloWorld() {
-		System.out.println("Hello World");
-	}
+import java.util.Date;
 
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.MediaType;
+
+
+
+@ApplicationPath("hello-world")
+public class HelloWorld extends Application{
+	
+		    @Path("hello")
+		    @GET
+		    @Produces(MediaType.APPLICATION_JSON)
+		    public String sayHello() {
+		    	Date dt= new Date();
+		        return "Hellow world  RESTful Web Services SE Bootstrap"+dt;
+		    }
+		    
+		   
 }
